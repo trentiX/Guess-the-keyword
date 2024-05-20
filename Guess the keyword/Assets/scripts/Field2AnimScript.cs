6d4455148627ef7d9eb11005cdf78c8a70694f70
Kaisar <130845163+trentiX@users.noreply.github.com>
2024-05-20 10:04:33 +0500

    Update Field2AnimScript.cs

--- Guess the keyword/Assets/scripts/Field2AnimScript.cs
+++ Guess the keyword/Assets/scripts/Field2AnimScript.cs
@@ -8,7 +8,7 @@ public class Field2AnimScript : MonoBehaviour
     public int x;
     public int y;   
     public int dest;
-    private void OnEnable()
+    private void OnEnable() // play animation on start of the game
     {
         Field.localPosition = new Vector2(x, y);
         Field.LeanMoveLocalX(dest, 0.75f).setEaseOutQuart().delay = 0.15f;
